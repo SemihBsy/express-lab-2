7a619f4338d282e546189c5be1eded0c174c444a
SemihB <semihr5171@gmail.com>
2022-11-03 22:21:06 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -11,10 +11,19 @@ app.get('/tip/:total/:tipPercentage', (request, response) => {
     response.send("The tip is " + (parseInt(request.params.total) * parseInt(request.params.tipPercentage) / parseInt(100)))
 })
 
+const ballResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
+
+app.get('/magic/:question', (request, response) => {
+    const randomResponse = ballResponses[Math.floor(Math.random() * ballResponses.length)]
+    response.send(`${request.params.question}` + `<h1>${randomResponse}</h1>`)
+})
 
 
 app.listen(PORT, () => {
     console.log('Express is listening on port: ', PORT)
 })
+
+
+
